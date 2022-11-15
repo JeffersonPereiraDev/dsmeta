60dcc79f0cee0674ee011205d2bcd798536918e8
Leminll <thiagopereiradelira233@hotmail.com>
2022-11-14 21:56:09 -0300

    Others components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,20 @@
 import './App.css'
 import NotificatinoButton from './components/NotificationButton'
+import Header from './components/Header'
+import SalesCard from './components/SalesCard'
 
 function App() {
   return (
-    <NotificatinoButton/>
+    <>
+      <main>
+        <section id="sales">
+          <div className="dsmeta-container">
+            <Header />
+            <SalesCard />
+          </div>
+        </section>
+      </main>
+    </>
   )
 }
 
